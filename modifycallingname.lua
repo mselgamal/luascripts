61552e5486b6d0f1873f5fba91fa9dac554be566
Mamdouh Elgamal <mselgamal2@gmail.com>
2017-01-20 16:43:32 -0500

    Update modifycallingname.lua

--- modifycallingname.lua
+++ modifycallingname.lua
@@ -1,1 +1,13 @@
 
+M = {}
+function M.inbound_INVITE(msg)
+  local from = msg:getHeader("From")
+  local ext = string.match(from,":(.+)@")
+  msg:removeHeader("Remote-Party-ID") // this line is not needed if there is no RPID in the invite
+  if ext == "<insert target calling number>" then
+    local tempfrom = string.match(from,"(<.+>.+)")
+    local from_with_calling_name = "\"<insert calling name>\" " .. tempfrom
+    msg:modifyHeader("From",from_with_calling_name)
+  end
+end
+return M
